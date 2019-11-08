9fd051d809a24e18e3374274142369020293d237
R00tSAM <49706361+R00tSAM@users.noreply.github.com>
2019-11-08 18:29:41 +0900

    Update Sender.php

--- Sender.php
+++ Sender.php
@@ -1,16 +1,16 @@
 <?php
 $name = $_POST['name'];
 $visitor_email = $_POST['email'];
-$message = $_POST['message'];
+$message = $_POST['url'];
 
 
-$email_from = 'EasyTutorials@avinashkr.com';
+$email_from = '?';
 
-$subject = "New Form Submission";
+$subject = "New Form Submit";
 
 $email_body = "User Name: $name.\n".
 "User Email: $visitor_email.\n".
-"User Message: $message.\n";
+"User Message: $url.\n";
 
 
 $to = "sardhot62@gmail.com";
