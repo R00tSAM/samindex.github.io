c20d666bd6e1905b66627c0e85d08b5c2d4c6399
R00tSAM <49706361+R00tSAM@users.noreply.github.com>
2019-11-09 12:46:17 +0900

    Update layout.js

--- layout.js
+++ layout.js
@@ -15,3 +15,19 @@ var bubblyButtons = document.getElementsByClassName("bubbly-button");
 for (var i = 0; i < bubblyButtons.length; i++) {
   bubblyButtons[i].addEventListener('click', animateButton, false);
 }
+
+(function (window, $) {
+  'use strict';
+
+  $.fn.useSound = function (_event, _id) {
+    var se = $(_id);
+    this.on(_event, function(){
+      se[0].currentTime = 0;
+      se[0].play();
+    });
+    return this;
+  };
+
+})(this, this.jQuery);
+
+$('.btn a').useSound('mousedown touchstart', '#sound');
