341896c68bd5e410edc2e3f09d623cf799b4f3a1
R00tSAM <49706361+R00tSAM@users.noreply.github.com>
2020-05-04 22:16:43 +0900

    test

--- script/animation.js
+++ script/animation.js
@@ -1,8 +1,17 @@
-$(function() {
+/*$(function() {
 	setTimeout(function(){
 		$('.start p').fadeIn(900);
 	},300);
 	setTimeout(function(){
 		$('.start').fadeOut(400);
 	},900);
+});*/
+
+$(function() {
+	setTimeout(function(){
+		$('.start p').fadeIn(1600);
+	},500); //0.5秒後にロゴをフェードイン!
+	setTimeout(function(){
+		$('.start').fadeOut(500);
+	},2500); //2.5秒後にロゴ含め真っ白背景をフェードアウト！
 });
